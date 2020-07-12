3669b61905edfb38787ed85fab0c028be0a41abe
Hugo Oshiro <hugo.oshiro@n26.com>
2020-07-12 20:01:27 +0200

    prod changes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
 import Game from "./game";
 import { WIDTH, HEIGHT } from "./game-params";
 
-const containerEl = document.getElementById("container");
-if (containerEl) {
-  containerEl.style.display = "grid";
-  containerEl.style.justifyContent = "center";
+function startSnake() {
+  const containerEl = document.getElementById("container");
+  if (containerEl) {
+    containerEl.style.display = "grid";
+    containerEl.style.justifyContent = "center";
+    containerEl.style.border;
+  }
+  const game = new Game(WIDTH, HEIGHT);
+  game.run();
 }
 
-const game = new Game(WIDTH, HEIGHT);
-game.run();
+const snakeItem = document.getElementById("snake");
+if (snakeItem) {
+  snakeItem.addEventListener("click", () => {
+    const gameSelector = document.getElementById("game-selector");
+    if (gameSelector) {
+      gameSelector.style.display = "none";
+    }
+    document.body.style.backgroundColor = "white";
+    startSnake();
+  });
+}
